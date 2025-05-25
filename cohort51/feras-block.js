d4c93decf175233c94192fcf528f966ecda9d88e
Jim Cramer <remarcmij@gmail.com>
2025-05-25 18:34:18 +0200

    Removed obsoleted files

--- cohort51/feras-block.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-  console.log('This should run after 0.5 seconds');
-}, 500);
-
-let start = Date.now();
-while (Date.now() - start < 3000) {
-  // do nothing
-}
-console.log('start');
